613fab3ab588a78562e5c3fedf5051861071b193
niravmore <68939506+niravmore@users.noreply.github.com>
2020-10-11 17:20:01 +0530

    Add files via upload

---
+++ main_calc.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class main_calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("unused")
+		calculator calcn = new calculator();
+
+	}
+
+}
